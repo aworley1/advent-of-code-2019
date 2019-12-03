a07da598329a6d9ce7389264c22576b64bcff77a
Andrew Worley <andrew.worley@johnlewis.co.uk>
2019-12-03 08:48:42 +0000

    D3P1 Parse input

--- src/main/kotlin/day3/Day3.kt
+++ src/main/kotlin/day3/Day3.kt
@@ -3,3 +3,24 @@ package day3
 fun solvePuzzle(input: List<String>): Int {
     return 0
 }
+
+fun parse(input: List<String>): List<List<Instruction>> {
+    return input.map { it.split(",") }
+        .map {
+            it.map {
+                Instruction(
+                    operation = Operation.valueOf(it.substring(0, 1)),
+                    amount = it.substring(1).toInt()
+                )
+            }
+        }
+}
+
+data class Instruction(val operation: Operation, val amount: Int)
+
+enum class Operation {
+    U,
+    D,
+    L,
+    R
+}
--- src/test/kotlin/day3/Day3Test.kt
+++ src/test/kotlin/day3/Day3Test.kt
@@ -2,10 +2,23 @@ package day3
 
 import assertk.assertThat
 import assertk.assertions.isEqualTo
+import day3.Operation.*
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 
 object Day3Test : Spek({
+    describe("parser") {
+        it("should parse list of strings into list of ops") {
+            val input = listOf("U1,D2", "L3,R4")
+
+            val expectedResult = listOf(
+                listOf(Instruction(U, 1), Instruction(D, 2)),
+                listOf(Instruction(L, 3), Instruction(R, 4))
+            )
+
+            assertThat(parse(input)).isEqualTo(expectedResult)
+        }
+    }
     describe("day3") {
         it("should calculate for test input1") {
             val input = listOf("R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83")
