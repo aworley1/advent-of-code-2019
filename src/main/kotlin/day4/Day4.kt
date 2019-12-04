aed17cc1268ee550303e0bee9985e7c6d87e5e45
Andrew Worley <andrew.worley@johnlewis.co.uk>
2019-12-04 09:10:26 +0000

    D4P2 Complete

--- src/main/kotlin/day4/Day4.kt
+++ src/main/kotlin/day4/Day4.kt
@@ -1,8 +1,31 @@
 package day4
 
+fun main() {
+    val validPasswords = (128392..643281)
+        .map { it.toString() }
+        .mapNotNull { if (validatePassword(it)) it else null }
+
+    val validPasswordsPart2 = (128392..643281)
+        .map { it.toString() }
+        .mapNotNull { if (validatePasswordPart2(it)) it else null }
+
+    println("${validPasswords.size} passwords are valid")
+    println("${validPasswordsPart2.size} part 2 passwords are valid")
+}
+
 fun validatePassword(password: String): Boolean {
     return when {
         isSixDigits(password) && containsRepeatingDigits(password) && onlyIncreases(password) -> true
+        else -> false
+    }
+}
+
+fun validatePasswordPart2(password: String): Boolean {
+    return when {
+        isSixDigits(password)
+                && containsRepeatingDigits(password)
+                && onlyIncreases(password)
+                && hasCorrectRepeatingDigits(password) -> true
         else -> false
     }
 }
@@ -17,4 +40,29 @@ private fun containsRepeatingDigits(password: String): Boolean {
 
 private fun onlyIncreases(password: String): Boolean {
     return password.windowed(2).all { it[0].toInt() <= it[1].toInt() }
+}
+
+private fun hasCorrectRepeatingDigits(password: String): Boolean {
+    return countRepeatingDigits(password).contains(2)
+}
+
+fun countRepeatingDigits(password: String): List<Int> {
+    val numbersOfRepeats = mutableListOf<Int>()
+    var numberOfRepeats = 0
+
+    password.forEachIndexed { index, c ->
+        val nextChar =
+            if (index != password.length - 1) {
+                password[index + 1]
+            } else null
+
+        if (c == nextChar)
+            numberOfRepeats++
+        else {
+            numbersOfRepeats.add(numberOfRepeats + 1)
+            numberOfRepeats = 0
+        }
+    }
+
+    return numbersOfRepeats.toList()
 }
--- src/test/kotlin/day4/Day4Test.kt
+++ src/test/kotlin/day4/Day4Test.kt
@@ -1,6 +1,7 @@
 package day4
 
 import assertk.assertThat
+import assertk.assertions.isEqualTo
 import assertk.assertions.isFalse
 import assertk.assertions.isTrue
 import org.spekframework.spek2.Spek
@@ -25,5 +26,43 @@ object Day4Test :  Spek({
             assertThat(validatePassword("111111")).isTrue()
         }
 
+    }
+
+    describe("part 2 password validator") {
+        it("should not validate if number is not 6 digits") {
+            assertThat(validatePasswordPart2("1123")).isFalse()
+            assertThat(validatePasswordPart2("112344444444")).isFalse()
+        }
+
+        it("should not validate if there are no repeating digits") {
+            assertThat(validatePasswordPart2("123456")).isFalse()
+        }
+
+        it("should not validate if there are digits which decrease") {
+            assertThat(validatePasswordPart2("113436")).isFalse()
+        }
+
+        it("should validate if there are repeating digits which never decrease") {
+            assertThat(validatePasswordPart2("112345")).isTrue()
+        }
+
+        it("should match test input 1") {
+            assertThat(validatePasswordPart2("112233")).isTrue()
+        }
+
+        it("should match test input 2") {
+            assertThat(validatePasswordPart2("123444")).isFalse()
+        }
+
+        it("should match test input 3") {
+            assertThat(validatePasswordPart2("111122")).isTrue()
+        }
+
+    }
+
+    describe("repeating digit counter") {
+        it("should correctly count groups of repeats") {
+            assertThat(countRepeatingDigits("1112222")).isEqualTo(listOf(3,4))
+        }
     }
 })
